23e6693f325e6476f15104c69e72febff04a97e9
LiveDream0322 <livedream0322@gmail.com>
2023-10-15 04:21:32 -0400

    change time-zone

--- server/controllers/betevoController.js
+++ server/controllers/betevoController.js
@@ -6,12 +6,12 @@ const getTotalGames = async () => {
   try {
     const currentDate = new Date();
     // Set the time to the start of the current day
-//    currentDate.setHours(-4, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(-4, 0, 0, 0);
+//    currentDate.setHours(0, 0, 0, 0);
     // Set the time to the end of the current day
     const endDate = new Date(currentDate);
-    endDate.setHours(23, 59, 59, 999);
-//    endDate.setHours(43, 59, 59, 999);
+//    endDate.setHours(23, 59, 59, 999);
+    endDate.setHours(43, 59, 59, 999);
     // Define the query
     const query = {
       game_datetime: {
@@ -31,12 +31,12 @@ const getBetevoSpread = async (req, res) => {
   try {
     const currentDate = new Date();
     // Set the time to the start of the current day
-//    currentDate.setHours(-4, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(-4, 0, 0, 0);
+//    currentDate.setHours(0, 0, 0, 0);
     // Set the time to the end of the current day
     const endDate = new Date(currentDate);
-    endDate.setHours(23, 59, 59, 999);
-//    endDate.setHours(43, 59, 59, 999);
+//    endDate.setHours(23, 59, 59, 999);
+    endDate.setHours(43, 59, 59, 999);
     // Define the query
     const query = {
       game_datetime: {
@@ -111,12 +111,12 @@ const getBetevoMoneyLine = async (req, res) => {
   try {
     const currentDate = new Date();
     // Set the time to the start of the current day
-//    currentDate.setHours(-4, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(-4, 0, 0, 0);
+//    currentDate.setHours(0, 0, 0, 0);
     // Set the time to the end of the current day
     const endDate = new Date(currentDate);
-    endDate.setHours(23, 59, 59, 999);
-//    endDate.setHours(43, 59, 59, 999);
+//    endDate.setHours(23, 59, 59, 999);
+    endDate.setHours(43, 59, 59, 999);
     // Define the query
     const query = {
       game_datetime: {
@@ -178,12 +178,12 @@ const getBetevoTotal = async (req, res) => {
   try {
     const currentDate = new Date();
     // Set the time to the start of the current day
-//    currentDate.setHours(-4, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(-4, 0, 0, 0);
+//    currentDate.setHours(0, 0, 0, 0);
     // Set the time to the end of the current day
     const endDate = new Date(currentDate);
-    endDate.setHours(23, 59, 59, 999);
-//    endDate.setHours(43, 59, 59, 999);
+//    endDate.setHours(23, 59, 59, 999);
+    endDate.setHours(43, 59, 59, 999);
     // Define the query
     const query = {
       game_datetime: {
